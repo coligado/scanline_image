e8c559d251154171a716185b6ead1fa1f80e87e9
coligado <coligado@cs.stanford.edu>
2015-10-11 11:59:39 -0700

    Uploaded assignment folder

---
+++ source/build/xcode/cs148-opengl4.build/Debug/ALL_BUILD.build/Script-9B25623FBA5B4862A4A641C5.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/leacoligado/Downloads/cs148opengl4/source/build/xcode -f /Users/leacoligado/Downloads/cs148opengl4/source/build/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ source/build/xcode/cs148-opengl4.build/Debug/ZERO_CHECK.build/Script-D9AC55A6E7C24F86824F5608.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/leacoligado/Downloads/cs148opengl4/source/build/xcode -f /Users/leacoligado/Downloads/cs148opengl4/source/build/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
